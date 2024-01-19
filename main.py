971256ca9c95019c5cb43ce223a5489e3604ca45
Gérald Arnould <gerald@agharta.fr>
2024-01-19 15:55:25 +0100

    feat: count characters occurences in a book

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ def open_book(path: str) -> str:
 def count_words(book: str) -> int:
     return len(book.split())
 
+def char_occurence(book):
+    occurences = {}
+    for c in book:
+        if c.lower() in occurences:
+            occurences[c.lower()] += 1
+        else:
+            occurences[c.lower()] = 1
+    return occurences
+
 def main() -> int:
     book = open_book("./books/frankenstein.txt")
     print(book)
     words = count_words(book)
     print(f"Book contains {words}.")
+    # letters occurence
+    occurences = char_occurence(book)
+    print(occurences)
     return 0
 
 if __name__ == '__main__':
