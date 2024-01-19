d289dc690c7f612565ac3597104af40fc73a6f18
Gérald Arnould <gerald@agharta.fr>
2024-01-19 16:16:05 +0100

    feat: aggregate stats in a report + sorting

--- main.py
+++ main.py
@@ -17,14 +17,37 @@ def char_occurence(book):
             occurences[c.lower()] = 1
     return occurences
 
-def main() -> int:
-    book = open_book("./books/frankenstein.txt")
-    print(book)
-    words = count_words(book)
-    print(f"Book contains {words}.")
-    # letters occurence
+def sorted_char_occurence(chars_dict):
+    sorted_occurences = []
+    for c in chars_dict:
+        if c.isalpha():
+            sorted_occurences.append((c, chars_dict[c]))
+    # Sort in reverse according to the second tuple member (char occurence)
+    # source: https://docs.python.org/3/howto/sorting.html
+    sorted_occurences.sort(reverse=True, key=lambda char: char[1])
+    return sorted_occurences
+
+def report(path):
+    print(f"--- Begin report of {path}")
+    book = open_book(path)
+    print(count_words(book), " words found in the document\n")
     occurences = char_occurence(book)
-    print(occurences)
+    sorted_occurences = sorted_char_occurence(occurences)
+    for occ in sorted_occurences:
+        print(f"The '{occ[0]}' char was found {occ[1]} times.")
+
+
+
+def main() -> int:
+    path = "./books/frankenstein.txt"
+    # book = open_book(path)
+    # print(book)
+    # words = count_words(book)
+    # print(f"Book contains {words}.")
+    # letters occurence
+    # occurences = char_occurence(book)
+    # print(occurences)
+    report(path)
     return 0
 
 if __name__ == '__main__':
