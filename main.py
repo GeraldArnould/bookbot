1501c6950c86eee2334472c79d761eae72cd32c5
Gérald Arnould <gerald@agharta.fr>
2024-01-19 15:32:05 +0100

    feat: count words in books

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ def open_book(path: str) -> str:
         file_contents = f.read()
         return file_contents
 
+def count_words(book: str) -> int:
+    return len(book.split())
+
 def main() -> int:
     book = open_book("./books/frankenstein.txt")
     print(book)
+    words = count_words(book)
+    print(f"Book contains {words}.")
     return 0
 
 if __name__ == '__main__':
