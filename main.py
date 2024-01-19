5fc5a28937c6d54fc72daf2ce529f54d0fdcdde1
Gérald Arnould <gerald@agharta.fr>
2024-01-19 15:27:51 +0100

    refactor: separate book opening and printing

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 
-def print_book(path: str) -> None:
+def open_book(path: str) -> str:
     with open(path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 def main() -> int:
-    print_book("./books/frankenstein.txt")
+    book = open_book("./books/frankenstein.txt")
+    print(book)
     return 0
 
 if __name__ == '__main__':
